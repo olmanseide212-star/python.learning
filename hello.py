85e29b94539e61fc04ca2843d7cf7de373cc4fe9
anno <anno@annodeMacBook.local>
2026-08-01 16:59:50 +0800

    保存Python基础学习进度：完成print、变量、数据类型、input等练习

---
+++ hello.py
@@ -0,0 +1,24 @@
+print("你好，世界！")
+print("Hello, World!")
+print("李常勇")
+print("你好")
+print("朋友")
+name = "遂川中学" 
+print(name)
+age = 18
+length = 180
+print(age)
+print(length)
+a = 10
+b = 2.5
+z = "10"
+print(a)
+print(b)
+print(z)
+print("花切")
+name="李常勇"
+print("我的名字是：" + name)
+age = "18"
+print("我的年龄是：" + age)
+user_name = input("请输入你的名字")
+print("你好，" + user_name)
